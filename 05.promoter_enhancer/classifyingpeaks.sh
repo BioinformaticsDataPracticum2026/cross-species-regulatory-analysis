154527fb79440dfef4d4c8f721e023e17866610f
Maitreyee Karne <mkarne@andrew.cmu.edu>
2026-04-13 07:55:00 -0400

    code to classify peaks into enhancers or promoters

---
+++ 05.promoter_enhancer/classifyingpeaks.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Load bedtools so we can compare genomic regions
+module load bedtools
+#we want to classify peaks  we have about 206765 human ATAC peaks.
+# if this peak is near  a tss we will classify it as prmoter if not then it is an enhancer
+echo "Classifying human peaks..."
+bedtools intersect -a human_adrenal_idr_optimal.narrowPeak -b human_tss_2kb.bed -u > humanpromoters.bed
+bedtools intersect -a human_adrenal_idr_optimal.narrowPeak -b human_tss_2kb.bed -v > humanenhancers.bed
+#now we will classify mouse peaks as promoters or enhancers
+echo "Classifying mouse peaks..."
+bedtools intersect -a mouse_adrenal_idr_optimal.narrowPeak -b mouse_tss_2kb.bed -u > mouse_promoters.bed
+bedtools intersect -a mouse_adrenal_idr_optimal.narrowPeak -b mouse_tss_2kb.bed -v > mouse_enhancers.bed
+
+
+echo "Human promoters:"; wc -l humanpromoters.bed
+echo "Human enhancers:"; wc -l humanenhancers.bed
+echo "Mouse promoters:"; wc -l mouse_promoters.bed
+echo "Mouse enhancers:"; wc -l mouse_enhancers.bed
+# from xingyu's task 2 we know which peaks are shared and specific
+#now we want to know among conserved how many are  promoters vs enhancers
+#we want to know how  many from the species specific are promoters vs enhacers
+echo "Checking conservation..."
+# Conserved peaks which are human peaks that have a mouse counterpart
+bedtools intersect -a human_adrenal_idr_optimal.HumanToMouse.shared_with_mouse_native.bed -b mouse_tss_2kb.bed -u > conservedpromoters.bed
+bedtools intersect -a human_adrenal_idr_optimal.HumanToMouse.shared_with_mouse_native.bed -b mouse_tss_2kb.bed -v > conservedenhancers.bed
+
+# Human-specific peaks which are human peaks with NO mouse counterpart
+bedtools intersect -a human_adrenal_idr_optimal.HumanToMouse.no_mouse_native_overlap.bed -b mouse_tss_2kb.bed -u > humanspecific_promoters.bed
+bedtools intersect -a human_adrenal_idr_optimal.HumanToMouse.no_mouse_native_overlap.bed -b mouse_tss_2kb.bed -v > humanspecific_enhancers.bed
+
+# Mouse-specific peaks which are mouse peaks with NO human counterpart
+bedtools intersect -a mouse_adrenal_idr_optimal.no_human_mapped_overlap.bed -b mouse_tss_2kb.bed -u > mousespecific_promoters.bed
+bedtools intersect -a mouse_adrenal_idr_optimal.no_human_mapped_overlap.bed -b mouse_tss_2kb.bed -v > mousespecific_enhancers.bed
+
+echo ""
+echo "Conserved promoters:"; wc -l conservedpromoters.bed
+echo "Conserved enhancers:"; wc -l conservedenhancers.bed
+echo "Human-specific promoters:"; wc -l humanspecific_promoters.bed
+echo "Human-specific enhancers:"; wc -l humanspecific_enhancers.bed
+echo "Mouse-specific promoters:"; wc -l mousespecific_promoters.bed
+echo "Mouse-specific enhancers:"; wc -l mousespecific_enhancers.bed
+
+
+
